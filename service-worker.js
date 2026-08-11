2132d5d31fd7f966359afac62f689e0436c1bc98
Trexia Olaya <147968406+Proftrex@users.noreply.github.com>
2026-08-11 13:01:04 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,114 @@
+const CACHE_NAME = "wattwise-cache-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+
+// INSTALL SERVICE WORKER
+self.addEventListener(
+  "install",
+  function(event) {
+
+    console.log(
+      "WattWise Service Worker Installed"
+    );
+
+    event.waitUntil(
+
+      caches.open(
+        CACHE_NAME
+      )
+      .then(
+        function(cache) {
+
+          return cache.addAll(
+            APP_FILES
+          );
+
+        }
+      )
+
+    );
+
+  }
+);
+
+
+// ACTIVATE SERVICE WORKER
+self.addEventListener(
+  "activate",
+  function(event) {
+
+    console.log(
+      "WattWise Service Worker Activated"
+    );
+
+
+    event.waitUntil(
+
+      caches.keys()
+      .then(
+        function(cacheNames) {
+
+          return Promise.all(
+
+            cacheNames.map(
+              function(cacheName) {
+
+                if(
+                  cacheName !== CACHE_NAME
+                ){
+
+                  return caches.delete(
+                    cacheName
+                  );
+
+                }
+
+              }
+            )
+
+          );
+
+        }
+      )
+
+    );
+
+  }
+);
+
+
+// FETCH FILES
+self.addEventListener(
+  "fetch",
+  function(event) {
+
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      )
+      .then(
+        function(response) {
+
+          return (
+            response ||
+            fetch(
+              event.request
+            )
+          );
+
+        }
+      )
+
+    );
+
+  }
+);
