ccb16cc634b49abbeb75b76bd46463f72b62c709
Trexia Olaya <147968406+Proftrex@users.noreply.github.com>
2026-08-25 13:56:00 +0000

    Add WattWise PWA support

--- service-worker.js
+++ service-worker.js
@@ -1,114 +1,60 @@
-const CACHE_NAME = "wattwise-cache-v2";
+const CACHE_NAME = "wattwise-cache-v3";
 
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
+  "./pouch-ui.css",
   "./scripts.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./assets/simplewattlogo.png",
+  "./assets/pwa/icon-192.png",
+  "./assets/pwa/icon-512.png"
 ];
 
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(APP_FILES);
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
+  );
+});
 
-// INSTALL SERVICE WORKER
-self.addEventListener(
-  "install",
-  function(event) {
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (cacheName) {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
+  );
+});
 
-    console.log(
-      "WattWise Service Worker Installed"
-    );
-
-    event.waitUntil(
-
-      caches.open(
-        CACHE_NAME
-      )
-      .then(
-        function(cache) {
-
-          return cache.addAll(
-            APP_FILES
-          );
-
-        }
-      )
-
-    );
-
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
   }
-);
 
-
-// ACTIVATE SERVICE WORKER
-self.addEventListener(
-  "activate",
-  function(event) {
-
-    console.log(
-      "WattWise Service Worker Activated"
-    );
-
-
-    event.waitUntil(
-
-      caches.keys()
-      .then(
-        function(cacheNames) {
-
-          return Promise.all(
-
-            cacheNames.map(
-              function(cacheName) {
-
-                if(
-                  cacheName !== CACHE_NAME
-                ){
-
-                  return caches.delete(
-                    cacheName
-                  );
-
-                }
-
-              }
-            )
-
-          );
-
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
         }
-      )
 
-    );
-
-  }
-);
-
-
-// FETCH FILES
-self.addEventListener(
-  "fetch",
-  function(event) {
-
-
-    event.respondWith(
-
-      caches.match(
-        event.request
-      )
-      .then(
-        function(response) {
-
-          return (
-            response ||
-            fetch(
-              event.request
-            )
-          );
-
-        }
-      )
-
-    );
-
-  }
-);
+        return fetch(event.request);
+      })
+  );
+});
